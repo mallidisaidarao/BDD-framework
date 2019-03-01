7d51abd8d61bdeda642c365319308968ca35e3f6
lukkaaruna <alukka@mavenstaffing.in>
2019-03-01 11:55:14 +0530

    hi guys

--- src/test/java/Tests/Gmailgluecode.java
+++ src/test/java/Tests/Gmailgluecode.java
@@ -7,6 +7,7 @@ import java.util.Properties;
 import org.junit.Assert;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
